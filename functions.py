98b1105a00676bddd270cb33ef00b82f5c394c63
Ryan Dahlke <ryandahlke@gmail.com>
2014-11-01 19:52:29 -0500

    beef and btc

---
+++ functions.py
@@ -0,0 +1,14 @@
+__author__ = 'Ryan'
+
+def beef():
+    print("It's what's for dinner.")
+
+def bitcoin_to_usd(btc):
+    amount = btc * 300
+    print(amount)
+
+
+
+beef()
+bitcoin_to_usd(46)
+bitcoin_to_usd(2)
